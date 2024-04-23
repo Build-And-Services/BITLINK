bd57330a8e6204a777e8a82497ea7bd797e158b0
Heri Setyawan <herisetyawan233@gmail.com>
2024-04-24 00:38:41 +0700

    feat: feature auth

--- routes/heri.php
+++ routes/heri.php
@@ -4,7 +4,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 Route::prefix('padi')->group(function(){
     Route::get('/', function (){
