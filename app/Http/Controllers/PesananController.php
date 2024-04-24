2c4ba352da704bf4f739d6c682b1527c9b009116
Faisal Bahari <fbhr1112@gmail.com>
2024-04-24 20:37:01 +0700

    fix: pesanan invoice

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -15,8 +15,23 @@ class PesananController extends Controller
     }
     public function invoice($id)
     {
-        $pesanan = Pesanan::find($id);
-        return view('frontend.pesanan.invoice', compact('pesanan'));
+        $pesanan = Pesanan::with(['pembeli', 'benihData'])->where('id', $id)->first();
+        $perusahaan = \DB::table('benih_data')
+            ->where('id_benih', $pesanan->id_benih)
+            ->join('users', 'users.id', '=', 'benih_data.id_akunp')
+            ->join('data_akun_produsen', 'data_akun_produsen.id_user', '=', 'users.id')
+            ->select([
+                'data_akun_produsen.nama_perusahaan',
+                'data_akun_produsen.nomor_legalitas_usaha',
+                'data_akun_produsen.alamat_lengkap'
+            ])->first();
+        return view(
+            'frontend.pesanan.invoice',
+            [
+                'getDetailInvoice' => $pesanan,
+                'getPerusahaan' => $perusahaan
+            ]
+        );
     }
     public function cekPengiriman(Request $request)
     {
@@ -34,5 +49,23 @@ class PesananController extends Controller
             ], 404);
         }
     }
+    public function updateStatusPengiriman(Request $request)
+    {
+        try {
+            \DB::beginTransaction();
+            $pesanan = Pesanan::find($request->id_pembayaran_result);
+            $pesanan->update([
+                'status_pengiriman' => 'DITERIMA',
+                'tgl_diterima' => now()
+            ]);
+            \DB::commit();
+            return response()->json([
+                'status_pengiriman' => $pesanan->status_pengiriman
+            ]);
+        } catch (\Throwable $th) {
+            \DB::rollBack();
+            return response()->json('failed');
+        }
 
+    }
 }
