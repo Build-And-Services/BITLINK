3e49fd98113f0a08ca13fcc3902382050b0220f7
Heri Setyawan <herisetyawan233@gmail.com>
2024-04-25 15:16:31 +0700

    feat: apply midtrans to transactions

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -43,6 +43,11 @@
         <link rel="stylesheet" href={{asset("css/responsive.css")}}>
 
 		{{-- <link rel="stylesheet" href={{asset("css/detail.css")}}> --}}
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- @TODO: replace SET_YOUR_CLIENT_KEY_HERE with your client key -->
+		<script type="text/javascript"
+			src="https://app.sandbox.midtrans.com/snap/snap.js"
+		data-client-key="{{ config('midtrans.client_key') }}"></script>
 
     </head>
     <body>
